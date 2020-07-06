120d6721ce691944ad25c8e18be6022d7a7edc59
Jovani Tarnowski <jovanitarnowski@gmail.com>
2020-07-05 21:11:42 -0300

    Config rss buzzfeed

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-let url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
+let url = 'https://www.buzzfeed.com/nifty.xml';
 
-const textarea = document.querySelector('#feed-textarea > ul');
+const textarea = document.querySelector('#feed-textarea');
 
 const date = new Date();
 document.querySelector('#date').innerHTML = date.toDateString();
 
 feednami.load(url)
 .then(feed => {
+    console.log(feed)
     textarea.value = ''
-    for(let entry of feed.entries){
+    for(let bob of feed.entries){
         //create a list element
         let li = document.createElement('li');
         //add HTML content to list items
-        li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
+        li.innerHTML = `<h4><img src=${bob.image.url}><a href="${bob.link}">${bob.title}</a></h4>`;
         //append HTML content to list 
         textarea.appendChild(li);
     }
 });
-
 
 //Using feednami to fetch RSS feeds
 //https://toolkit.sekando.com/docs/en/feednami
 
 //Feeds from BBC News
 //https://www.bbc.com/news/10628494#userss
-  
